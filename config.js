19f8434b3a9803801adf1eeda134efb93fdbea87
Md-Irshad-Alam <almairsh0205@gmail.com>
2024-04-06 14:43:31 +0530

    update

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const config = {
   Secret_key: '%$#%$^Hh',
-  Connect_db_URL: 'mongodb://localhost:27017/app',
+  Connect_db_URL:
+    'mongodb+srv://almairsh0205:Vyl3diYmd3BTalhT@cluster0.otro2lv.mongodb.net/',
 };
 module.exports = config;
 // 'mongodb+srv://Alam:alam123@cluster0.2w6rsvv.mongodb.net/inventryOrders?retryWrites=true&w=majority',
