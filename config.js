533882092929cec638dbad144af8e59017a125d4
Md irshad Alam <113013301+Md-Irshad-Alam@users.noreply.github.com>
2024-04-06 14:43:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ const config = {
   Connect_db_URL: 'mongodb://localhost:27017/app',
 };
 module.exports = config;
-// 'mongodb+srv://Alam:alam123@cluster0.2w6rsvv.mongodb.net/inventryOrders?retryWrites=true&w=majority',
+
